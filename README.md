a4e23ba0e84149fe256f3239d67e8acd27280d8a
Darius Schefer <darius1702@gmail.com>
2023-03-31 23:01:09 +0200

    Custom version

---
+++ README.md
@@ -0,0 +1,6 @@
+# dmenu
+Custom version of [dmenu](https://tools.suckless.org/dmenu/)
+
+## Changes
+- Colors to match [my dotfiles](https://github.com/darius1702/dotfiles)
+- [grid patch](https://tools.suckless.org/dmenu/patches/grid/)
--- config.def.h
+++ config.def.h
@@ -4,17 +4,18 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Iosevka Nerd Font Mono:size=14:style=Regular"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#E1E1E1", "#141414" },
+	[SchemeSel] = { "#141414", "#7FBBB3" },
+	[SchemeOut] = { "#141414", "#E67E80" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
+static unsigned int columns    = 0;
 
 /*
  * Characters not considered part of a word while deleting words
